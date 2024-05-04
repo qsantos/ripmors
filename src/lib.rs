54859168eb72f5512b304e951e31c1bb1c47197d
Quentin Santos <qsantos@qsantos.fr>
2024-05-04 12:56:30 +0200

    Switch to match for ASCII encoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod mappings;
 
-use mappings::ASCII_TO_MORSE;
+use mappings::ascii_to_morse;
 
 pub fn ascii_encode(s: &str) -> String {
-    let parts: Vec<&str> = s.as_bytes().iter().map(|b| ASCII_TO_MORSE[*b as usize]).collect();
+    let parts: Vec<&str> = s.as_bytes().iter().map(|b| ascii_to_morse(*b)).collect();
     parts.join(" ")
 }
--- src/mappings.rs
+++ src/mappings.rs
@@ -1,124 +1,118 @@
-pub const ASCII_TO_MORSE: [&str; 256] = {
-    // NOTE: ASCII only covers bytes values from 0 to 127, but we make the mapping from 0 to 255
-    // for convenience when iterating over bytes
-    let mut v = [""; 256];
+pub fn ascii_to_morse(c: u8) -> &'static str {
+    match c {
+        // NOTE: look for `=> "",` for missing characters
 
-    // NOTE: look for `= "";" for missing characters
+        b' ' => "/",
 
-    // 32 first control characters
+        b'!' => "..--.", // non standard: mapped to interrogation mark 
+        b'"' => ".-..-.", // Straight quotes (1.1.3)
+        b'#' => "",
+        b'$' => "...-..-", // non standard
+        b'%' => "----- -..-. -----", // (3.3.1)
+        b'&' => ". ...", // non standard: mapped to "es"
 
-    v[b' ' as usize] = "/";
+        b'\'' => ".----.", // Apostrophe (1.1.3)
+        b'(' => "-.--.", // Left-hand bracket (parenthesis) (1.1.3)
+        b')' => "-.--.-", // Right-hand bracket (parenthesis) (1.1.3)
+        b'*' => "-..-", // Multiplication sign (same as letter X) (1.1.3)
+        b'+' => ".-.-.", // Cross or addition sign (1.1.3)
+        b',' => "--..--", // Comma (1.1.3)
+        b'-' => "-....-", // Hyphen (1.1.3)
+        b'.' => ".-.-.-", // Full stop (period) (1.1.3)
+        b'/' => "-..-.", // Fraction bar or division sign (1.1.3)
 
-    v[b'!' as usize] = "..--."; // non standard: mapped to interrogation mark 
-    v[b'"' as usize] = ".-..-."; // Straight quotes (1.1.3)
-    v[b'#' as usize] = "";
-    v[b'$' as usize] = "...-..-"; // non standard
-    v[b'%' as usize] = "----- -..-. -----"; // (3.3.1)
-    v[b'&' as usize] = ". ..."; // non standard: mapped to "es"
+        // International Morse code, as per ITU-R M.1677-1
+        //
+        // 1.1.2. Figures (Hindu-Arab digits)
+        b'0' => "-----",
+        b'1' => ".----",
+        b'2' => "..---",
+        b'3' => "...--",
+        b'4' => "....-",
+        b'5' => ".....",
+        b'6' => "-....",
+        b'7' => "--...",
+        b'8' => "---..",
+        b'9' => "----.",
 
-    v[b'\'' as usize] = ".----."; // Apostrophe (1.1.3)
-    v[b'(' as usize] = "-.--."; // Left-hand bracket (parenthesis) (1.1.3)
-    v[b')' as usize] = "-.--.-"; // Right-hand bracket (parenthesis) (1.1.3)
-    v[b'*' as usize] = "-..-"; // Multiplication sign (same as letter X) (1.1.3)
-    v[b'+' as usize] = ".-.-."; // Cross or addition sign (1.1.3)
-    v[b',' as usize] = "--..--"; // Comma (1.1.3)
-    v[b'-' as usize] = "-....-"; // Hyphen (1.1.3)
-    v[b'.' as usize] = ".-.-.-"; // Full stop (period) (1.1.3)
-    v[b'/' as usize] = "-..-."; // Fraction bar or division sign (1.1.3)
+        b':' => "---...", // Colon r division sign (1.1.3)
+        b';' => "-.-.-.", // non standard
+        b'<' => "",
+        b'=' => "-...-",  // Double hyphen (1.1.3)
+        b'>' => "",
+        b'?' => "..--..", // Question mark (1.1.3)
+        b'@' => ".--.-.", // Commercial at (1.1.3)
 
-    // International Morse code, as per ITU-R M.1677-1
-    //
-    // 1.1.2. Figures (Hindu-Arab digits)
-    v[b'0' as usize] = "-----";
-    v[b'1' as usize] = ".----";
-    v[b'2' as usize] = "..---";
-    v[b'3' as usize] = "...--";
-    v[b'4' as usize] = "....-";
-    v[b'5' as usize] = ".....";
-    v[b'6' as usize] = "-....";
-    v[b'7' as usize] = "--...";
-    v[b'8' as usize] = "---..";
-    v[b'9' as usize] = "----.";
+        // 1. Morse code signals
+        // 1.1.1. Letters (Latins cript)
+        // Uppercase
+        b'A' => ".-",
+        b'B' => "-...",
+        b'C' => "-.-.",
+        b'D' => "-..",
+        b'E' => ".",
+        b'F' => "..-.",
+        b'G' => "--.",
+        b'H' => "....",
+        b'I' => "..",
+        b'J' => ".---",
+        b'K' => "-.-",
+        b'L' => ".-..",
+        b'M' => "--",
+        b'N' => "-.",
+        b'O' => "---",
+        b'P' => ".--.",
+        b'Q' => "--.-",
+        b'R' => ".-.",
+        b'S' => "...",
+        b'T' => "-",
+        b'U' => "..-",
+        b'V' => "...-",
+        b'W' => ".--",
+        b'X' => "-..-",
+        b'Y' => "-.--",
+        b'Z' => "--..",
 
-    v[b':' as usize] = "---..."; // Colon r division sign (1.1.3)
-    v[b';' as usize] = "-.-.-."; // non standard
-    v[b'<' as usize] = "";
-    v[b'=' as usize] = "-...-";  // Double hyphen (1.1.3)
-    v[b'>' as usize] = "";
-    v[b'?' as usize] = "..--.."; // Question mark (1.1.3)
-    v[b'@' as usize] = ".--.-."; // Commercial at (1.1.3)
+        b'[' => "",
+        b'\\' => "",
+        b']' => "",
+        b'^' => "",
+        b'_' => "",
+        b'`' => ".-----.", // non standard
 
-    // 1. Morse code signals
-    // 1.1.1. Letters (Latins cript)
-    // Uppercase
-    v[b'A' as usize] = ".-";
-    v[b'B' as usize] = "-...";
-    v[b'C' as usize] = "-.-.";
-    v[b'D' as usize] = "-..";
-    v[b'E' as usize] = ".";
-    v[b'F' as usize] = "..-.";
-    v[b'G' as usize] = "--.";
-    v[b'H' as usize] = "....";
-    v[b'I' as usize] = "..";
-    v[b'J' as usize] = ".---";
-    v[b'K' as usize] = "-.-";
-    v[b'L' as usize] = ".-..";
-    v[b'M' as usize] = "--";
-    v[b'N' as usize] = "-.";
-    v[b'O' as usize] = "---";
-    v[b'P' as usize] = ".--.";
-    v[b'Q' as usize] = "--.-";
-    v[b'R' as usize] = ".-.";
-    v[b'S' as usize] = "...";
-    v[b'T' as usize] = "-";
-    v[b'U' as usize] = "..-";
-    v[b'V' as usize] = "...-";
-    v[b'W' as usize] = ".--";
-    v[b'X' as usize] = "-..-";
-    v[b'Y' as usize] = "-.--";
-    v[b'Z' as usize] = "--..";
+        // Lowercase
+        b'a' => ".-",
+        b'b' => "-...",
+        b'c' => "-.-.",
+        b'd' => "-..",
+        b'e' => ".",
+        b'f' => "..-.",
+        b'g' => "--.",
+        b'h' => "....",
+        b'i' => "..",
+        b'j' => ".---",
+        b'k' => "-.-",
+        b'l' => ".-..",
+        b'm' => "--",
+        b'n' => "-.",
+        b'o' => "---",
+        b'p' => ".--.",
+        b'q' => "--.-",
+        b'r' => ".-.",
+        b's' => "...",
+        b't' => "-",
+        b'u' => "..-",
+        b'v' => "...-",
+        b'w' => ".--",
+        b'x' => "-..-",
+        b'y' => "-.--",
+        b'z' => "--..",
 
-    v[b'[' as usize] = "";
-    v[b'\\' as usize] = "";
-    v[b']' as usize] = "";
-    v[b'^' as usize] = "";
-    v[b'_' as usize] = "";
-    v[b'`' as usize] = ".-----."; // non standard
+        b'{' => "",
+        b'|' => "",
+        b'}' => "",
+        b'~' => "",
 
-    // Lowercase
-    v[b'a' as usize] = ".-";
-    v[b'b' as usize] = "-...";
-    v[b'c' as usize] = "-.-.";
-    v[b'd' as usize] = "-..";
-    v[b'e' as usize] = ".";
-    v[b'f' as usize] = "..-.";
-    v[b'g' as usize] = "--.";
-    v[b'h' as usize] = "....";
-    v[b'i' as usize] = "..";
-    v[b'j' as usize] = ".---";
-    v[b'k' as usize] = "-.-";
-    v[b'l' as usize] = ".-..";
-    v[b'm' as usize] = "--";
-    v[b'n' as usize] = "-.";
-    v[b'o' as usize] = "---";
-    v[b'p' as usize] = ".--.";
-    v[b'q' as usize] = "--.-";
-    v[b'r' as usize] = ".-.";
-    v[b's' as usize] = "...";
-    v[b't' as usize] = "-";
-    v[b'u' as usize] = "..-";
-    v[b'v' as usize] = "...-";
-    v[b'w' as usize] = ".--";
-    v[b'x' as usize] = "-..-";
-    v[b'y' as usize] = "-.--";
-    v[b'z' as usize] = "--..";
-
-    v[b'{' as usize] = "";
-    v[b'|' as usize] = "";
-    v[b'}' as usize] = "";
-    v[b'~' as usize] = "";
-
-    // last control character (DEL)
-
-    v
-};
+        _ => "", // 33 control characters
+    }
+}
