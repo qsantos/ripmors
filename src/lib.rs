0e4ccbea8f697c0dc2d7abce73c0ab6ebaa954e8
Quentin Santos <qsantos@qsantos.fr>
2024-05-04 13:40:11 +0200

    Skip collecting unknown characters

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,20 @@ mod mappings;
 use mappings::{ascii_to_morse, unicode_to_morse};
 
 pub fn ascii_encode(s: &str) -> String {
-    let parts: Vec<&str> = s.as_bytes().iter().map(|b| ascii_to_morse(*b)).collect();
+    let parts: Vec<&str> = s
+        .as_bytes()
+        .iter()
+        .map(|b| ascii_to_morse(*b))
+        .filter(|&x| x != "")
+        .collect();
     parts.join(" ")
 }
 
 pub fn unicode_encode(s: &str) -> String {
-    let parts: Vec<&str> = s.chars().map(|b| unicode_to_morse(b)).collect();
+    let parts: Vec<&str> = s
+        .chars()
+        .map(|b| unicode_to_morse(b))
+        .filter(|&x| x != "")
+        .collect();
     parts.join(" ")
 }
