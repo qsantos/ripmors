f99c30c671b7dd6bc288c5742f92da68bc7ea13f
Quentin Santos <qsantos@qsantos.fr>
2024-05-05 12:29:58 +0200

    Basic streaming

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,14 @@ pub fn ascii_encode(s: &[u8]) -> String {
     parts.join(" ")
 }
 
+pub fn ascii_encode_vec_u8(s: &[u8], buf: &mut Vec<u8>) {
+    buf.extend(ascii_to_morse(s[0]).as_bytes());
+    for c in &s[1..] {
+        buf.push(b' ');
+        buf.extend(ascii_to_morse(*c).as_bytes());
+    }
+}
+
 pub fn unicode_encode(s: &str) -> String {
     let parts: Vec<&str> = s
         .chars()
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use ripmors::ascii_encode;
+use std::io::{Read, Write};
+
+use ripmors::ascii_encode_vec_u8;
 
 fn main() {
-    println!("{}", ascii_encode(b"Hello, world!"));
+    let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    let mut input_buf = [0u8; 1 << 15];
+    let mut output_buf = Vec::new();
+    loop {
+        let n = stdin.read(&mut input_buf).unwrap();
+        output_buf.clear();
+        ascii_encode_vec_u8(&input_buf[..n], &mut output_buf);
+        stdout.write(&output_buf).unwrap();
+    }
 }
