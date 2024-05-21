cdb87c8dccc2dfe8abd9c9ed3292845ae031deee
Quentin Santos <qsantos@qsantos.fr>
2024-05-21 19:22:42 +0200

    Stop on EOF

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ fn main() {
     let mut input_buf = [0u8; 1 << 15];
     loop {
         let n = stdin.read(&mut input_buf).unwrap();
+        if n == 0 {
+            break;
+        }
         ascii_encode_to_writer(&mut buf_writer, &input_buf[..n]).unwrap();
     }
 }
