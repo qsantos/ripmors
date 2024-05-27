d4784d5c2738182afc5b1958cbefa571da5d9b49
Quentin Santos <qsantos@qsantos.fr>
2024-05-27 21:03:01 +0200

    Add command-line argument to select Morse variant

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,32 @@ use std::io::{BufWriter, Read};
 
 use ripmors::ascii_encode_to_writer;
 
+use clap::{Parser, ValueEnum};
+
+#[derive(ValueEnum, Debug, Default, Clone, Copy, Eq, Parser, PartialEq)]
+#[clap(rename_all = "kebab_case")]
+enum MorseVariant {
+    #[default]
+    Standard,
+    Greek,
+    Russian,
+    Japanese,
+    Korean,
+    Hebrew,
+    Arabic,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, num_args = 0..=1, default_missing_value = "standard")]
+    decode: Option<MorseVariant>,
+}
+
 fn main() {
+    let args = Args::parse();
+    dbg!(args);
+
     let mut stdin = std::io::stdin();
     let stdout = std::io::stdout();
 
