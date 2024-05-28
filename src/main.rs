53913bda4676a32e41f026f661d147a9841392de
Quentin Santos <qsantos@qsantos.fr>
2024-05-28 21:56:08 +0200

    Decoding streaming UTF-8

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    dbg!(args);
 
     let mut stdin = std::io::stdin();
     let stdout = std::io::stdout();
@@ -34,11 +33,34 @@ fn main() {
     let mut buf_writer = BufWriter::new(stdout);
 
     let mut input_buf = [0u8; 1 << 15];
-    loop {
-        let n = stdin.read(&mut input_buf).unwrap();
-        if n == 0 {
-            break;
+
+    if let Some(variant) = args.decode {
+        dbg!(variant);
+        let mut bytes_read = 0;
+        loop {
+            bytes_read += stdin.read(&mut input_buf[bytes_read..]).unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            let s = match std::str::from_utf8(&input_buf[..bytes_read]) {
+                Ok(s) => s,
+                Err(e) => {
+                    let bytes_decoded = e.valid_up_to();
+                    unsafe { std::str::from_utf8_unchecked(&input_buf[..bytes_decoded]) }
+                }
+            };
+            print!("{s}");
+            let bytes_decoded = s.bytes().len();
+            input_buf.copy_within(bytes_decoded..bytes_read, 0);
+            bytes_read -= bytes_decoded;
         }
-        ascii_encode_to_writer(&mut buf_writer, &input_buf[..n]).unwrap();
+    } else {
+        loop {
+            let n = stdin.read(&mut input_buf).unwrap();
+            if n == 0 {
+                break;
+            }
+            ascii_encode_to_writer(&mut buf_writer, &input_buf[..n]).unwrap();
+        }
     }
 }
