55ff24e2c191198a184e96da76d8a3ebe6aab6cd
Quentin Santos <qsantos@qsantos.fr>
2024-06-01 23:07:10 +0200

    Extract main feature into functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{BufWriter, Read};
+use std::io::{BufWriter, Read, Write};
 
 use ripmors::*;
 
@@ -24,6 +24,41 @@ struct Args {
     decode: Option<MorseVariant>,
 }
 
+fn encode_stream<R: Read, W: Write>(i: &mut R, o: &mut W) {
+    let mut input_buf = vec![0u8; 1 << 15];
+    loop {
+        let n = i.read(&mut input_buf).unwrap();
+        if n == 0 {
+            break;
+        }
+        ascii_encode_to_writer(o, &input_buf[..n]).unwrap();
+    }
+}
+
+fn decode_stream<R: Read, W: Write, F: Fn(&str) -> char>(i: &mut R, o: &mut W, char_decode: &F) {
+    let mut input_buf = vec![0u8; 1 << 15];
+    let mut bytes_read = 0;
+    loop {
+        bytes_read += i.read(&mut input_buf[bytes_read..]).unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        let s = match std::str::from_utf8(&input_buf[..bytes_read]) {
+            Ok(s) => s,
+            Err(e) => {
+                let bytes_decoded = e.valid_up_to();
+                unsafe { std::str::from_utf8_unchecked(&input_buf[..bytes_decoded]) }
+            }
+        };
+
+        morse_decode_to_writer(o, s, char_decode).unwrap();
+
+        let bytes_decoded = s.bytes().len();
+        input_buf.copy_within(bytes_decoded..bytes_read, 0);
+        bytes_read -= bytes_decoded;
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -31,8 +66,6 @@ fn main() {
     let stdout = std::io::stdout();
 
     let mut buf_writer = BufWriter::new(stdout);
-
-    let mut input_buf = vec![0u8; 1 << 15];
 
     if let Some(variant) = args.decode {
         let char_decode = match variant {
@@ -44,33 +77,8 @@ fn main() {
             MorseVariant::Hebrew => morse_to_hebrew,
             MorseVariant::Arabic => morse_to_arabic,
         };
-        let mut bytes_read = 0;
-        loop {
-            bytes_read += stdin.read(&mut input_buf[bytes_read..]).unwrap();
-            if bytes_read == 0 {
-                break;
-            }
-            let s = match std::str::from_utf8(&input_buf[..bytes_read]) {
-                Ok(s) => s,
-                Err(e) => {
-                    let bytes_decoded = e.valid_up_to();
-                    unsafe { std::str::from_utf8_unchecked(&input_buf[..bytes_decoded]) }
-                }
-            };
-
-            morse_decode_to_writer(&mut buf_writer, s, &char_decode).unwrap();
-
-            let bytes_decoded = s.bytes().len();
-            input_buf.copy_within(bytes_decoded..bytes_read, 0);
-            bytes_read -= bytes_decoded;
-        }
+        decode_stream(&mut stdin, &mut buf_writer, &char_decode);
     } else {
-        loop {
-            let n = stdin.read(&mut input_buf).unwrap();
-            if n == 0 {
-                break;
-            }
-            ascii_encode_to_writer(&mut buf_writer, &input_buf[..n]).unwrap();
-        }
+        encode_stream(&mut stdin, &mut buf_writer);
     }
 }
