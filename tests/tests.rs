04e73a78175b4c563971cb4e72fe59438baf8514
Quentin Santos <qsantos@qsantos.fr>
2024-06-02 10:34:47 +0200

    Fix test not checking for extra or missing lines

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,8 +5,21 @@ use ripmors::{decode_stream, encode_stream, morse_to_standard};
 fn compare_output_to_oracle(writer: BufWriter<Vec<u8>>, expected_filename: &str) {
     let output = String::from_utf8(writer.into_inner().unwrap()).unwrap();
     let expected = std::fs::read_to_string(expected_filename).unwrap();
-    for (l1, l2) in expected.lines().zip(output.lines()) {
-        assert_eq!(l1, l2, "Second-string should be same as first string");
+    let mut output_lines = output.lines();
+    let mut expected_lines = expected.lines();
+    loop {
+        match (expected_lines.next(), output_lines.next()) {
+            (Some(l1), Some(l2)) => {
+                assert_eq!(l1, l2, "Second-string should be same as first string");
+            }
+            (Some(l1), None) => {
+                panic!("Output stops before expected line: {:?}", l1);
+            }
+            (None, Some(l2)) => {
+                panic!("Output has extra lines starting with: {}", l2);
+            }
+            (None, None) => break,
+        }
     }
 }
 
