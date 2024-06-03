411391372e3d9eb739e1f471d4d43fae55746408
Quentin Santos <qsantos@qsantos.fr>
2024-06-03 19:20:22 +0200

    Rename MorseVariant to DecodeVariant

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::{Parser, ValueEnum};
 
 #[derive(ValueEnum, Debug, Default, Clone, Copy, Eq, Parser, PartialEq)]
 #[clap(rename_all = "kebab_case")]
-enum MorseVariant {
+enum DecodeVariant {
     #[default]
     Standard,
     Greek,
@@ -19,7 +19,7 @@ enum MorseVariant {
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, num_args = 0..=1, default_missing_value = "standard")]
-    decode: Option<MorseVariant>,
+    decode: Option<DecodeVariant>,
 }
 
 fn main() {
@@ -30,13 +30,13 @@ fn main() {
 
     if let Some(variant) = args.decode {
         let char_decode = match variant {
-            MorseVariant::Standard => morse_to_standard,
-            MorseVariant::Greek => morse_to_greek,
-            MorseVariant::Russian => morse_to_russian,
-            MorseVariant::Japanese => morse_to_japanese,
-            MorseVariant::Korean => morse_to_korean,
-            MorseVariant::Hebrew => morse_to_hebrew,
-            MorseVariant::Arabic => morse_to_arabic,
+            DecodeVariant::Standard => morse_to_standard,
+            DecodeVariant::Greek => morse_to_greek,
+            DecodeVariant::Russian => morse_to_russian,
+            DecodeVariant::Japanese => morse_to_japanese,
+            DecodeVariant::Korean => morse_to_korean,
+            DecodeVariant::Hebrew => morse_to_hebrew,
+            DecodeVariant::Arabic => morse_to_arabic,
         };
         decode_stream(&mut stdin, &mut stdout, &char_decode);
     } else {
