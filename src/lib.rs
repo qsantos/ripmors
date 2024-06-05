a0434f9a4c918b33d0d703cc9e947ba97c6b181d
Quentin Santos <qsantos@qsantos.fr>
2024-06-05 08:23:25 +0200

    Also assume bytes is exactly 8 bytes

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,11 @@ pub fn ascii_encode_to_writer<W: Write>(
             if (*c == b'\t' || *c == b'\n' || *c == b'\r') && cur > 0 && buf[cur - 1] == b' ' {
                 cur -= 1;
             }
-            unsafe { buf.get_unchecked_mut(cur..cur + 8) }.copy_from_slice(bytes);
+            unsafe {
+                let buf8 = buf.get_unchecked_mut(cur..cur + 8);
+                let bytes8 = &*(bytes.as_ptr() as *const [u8; 8]);
+                buf8.copy_from_slice(bytes8);
+            };
         } else {
             buf[cur..cur + len].copy_from_slice(bytes);
         }
