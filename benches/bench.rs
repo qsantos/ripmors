2769f90be056e9c448a32c9d63bdd15d43a11099
Quentin Santos <qsantos@qsantos.fr>
2024-06-08 18:30:26 +0200

    Add Criterion benchmark for decoding

--- benches/bench.rs
+++ benches/bench.rs
@@ -46,5 +46,26 @@ fn standard_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, ascii_benchmark, standard_benchmark);
+fn decode_benchmark(c: &mut Criterion) {
+    let data = std::fs::read_to_string("2-encoded.txt").unwrap();
+    let mut f = std::fs::File::open("2-encoded.txt").unwrap();
+    let mut devnull = std::fs::File::create("/dev/null").unwrap();
+    let mut group = c.benchmark_group("Unicode");
+    group.throughput(Throughput::Bytes(data.len() as u64));
+
+    group.bench_function("decode_string", |b| {
+        b.iter(|| morse_decode_to_string(black_box(&data.as_bytes()), morse_to_standard))
+    });
+
+    group.bench_function("decode_stream", |b| {
+        b.iter(|| {
+            f.rewind().unwrap();
+            decode_stream(&mut f, &mut devnull, morse_to_standard);
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, ascii_benchmark, standard_benchmark, decode_benchmark);
 criterion_main!(benches);
