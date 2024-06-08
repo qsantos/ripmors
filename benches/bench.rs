e724eafea687c0ca2a7dca2500bcfb4f81a57392
Quentin Santos <qsantos@qsantos.fr>
2024-06-08 17:51:16 +0200

    Use same data in Criterion as in bench script

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,14 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
-use rand::{distributions::Standard, Rng};
 
 use ripmors::{ascii_encode_to_string, standard_encode_to_string};
 
 fn ascii_benchmark(c: &mut Criterion) {
-    let ascii: String = rand::thread_rng()
-        .sample_iter::<u8, _>(Standard)
-        .take(1048576)
-        .map(|c| c as char)
-        .collect();
+    let ascii = std::fs::read_to_string("1-original.txt").unwrap();
     let mut group = c.benchmark_group("ASCII");
     group.throughput(Throughput::Bytes(ascii.len() as u64));
     group.bench_function("encode", |b| {
@@ -18,7 +13,7 @@ fn ascii_benchmark(c: &mut Criterion) {
 }
 
 fn standard_benchmark(c: &mut Criterion) {
-    let data = std::fs::read_to_string("benches/unicode_test_page.txt").unwrap();
+    let data = std::fs::read_to_string("4-unicode.txt").unwrap();
     let mut group = c.benchmark_group("Unicode");
     group.throughput(Throughput::Bytes(data.len() as u64));
     group.bench_function("encode", |b| {
