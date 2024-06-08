80b7ec35cc6e65c88cdfe6c8798a7596cdd7b398
Quentin Santos <qsantos@qsantos.fr>
2024-06-08 18:06:24 +0200

    Add Criterion benchmarks for encode_stream

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,24 +1,48 @@
+use std::io::Seek;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 
-use ripmors::{ascii_encode_to_string, standard_encode_to_string};
+use ripmors::*;
 
 fn ascii_benchmark(c: &mut Criterion) {
-    let ascii = std::fs::read_to_string("1-original.txt").unwrap();
+    let data = std::fs::read_to_string("1-original.txt").unwrap();
+    let mut f = std::fs::File::open("1-original.txt").unwrap();
+    let mut devnull = std::fs::File::create("/dev/null").unwrap();
     let mut group = c.benchmark_group("ASCII");
-    group.throughput(Throughput::Bytes(ascii.len() as u64));
-    group.bench_function("encode", |b| {
-        b.iter(|| ascii_encode_to_string(black_box(&ascii)))
+    group.throughput(Throughput::Bytes(data.len() as u64));
+
+    group.bench_function("encode_string", |b| {
+        b.iter(|| ascii_encode_to_string(black_box(&data)))
     });
+
+    group.bench_function("encode_stream", |b| {
+        b.iter(|| {
+            f.rewind().unwrap();
+            encode_stream_ascii(&mut f, &mut devnull);
+        })
+    });
+
     group.finish();
 }
 
 fn standard_benchmark(c: &mut Criterion) {
     let data = std::fs::read_to_string("4-unicode.txt").unwrap();
+    let mut f = std::fs::File::open("4-unicode.txt").unwrap();
+    let mut devnull = std::fs::File::create("/dev/null").unwrap();
     let mut group = c.benchmark_group("Unicode");
     group.throughput(Throughput::Bytes(data.len() as u64));
-    group.bench_function("encode", |b| {
+
+    group.bench_function("encode_string", |b| {
         b.iter(|| standard_encode_to_string(black_box(&data)))
     });
+
+    group.bench_function("encode_stream", |b| {
+        b.iter(|| {
+            f.rewind().unwrap();
+            encode_stream_standard(&mut f, &mut devnull);
+        })
+    });
+
     group.finish();
 }
 
